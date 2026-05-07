4c40b8d3c583a700aabb2c54bca28bce0e1853d3
Mudasir <151111505+Mudasir24@users.noreply.github.com>
2026-05-07 06:19:36 +0530

    Update file path for video download script

--- Dataset_Download/download_videos.py
+++ Dataset_Download/download_videos.py
@@ -1,6 +1,6 @@
 import subprocess
 
-with open(r"C:\\Users\\Mohammed Mudasir\\OneDrive\\Desktop\\Major Project\\Towards-Automatic-Speech-to-SL\\Dataset\\train.txt") as f:
+with open("Dataset/train.txt") as f:
     for line in f:
         vid = line.strip()
         url = f"https://www.youtube.com/watch?v={vid}"
